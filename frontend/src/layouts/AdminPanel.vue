9ea4623e5d53d48db009ce8cbbfc178491054a3c
Franpall <skfranpal@gmail.com>
2025-04-27 10:18:24 -0400

    Creación de menú lateral en el Admin Layout

--- frontend/src/layouts/AdminPanel.vue
+++ frontend/src/layouts/AdminPanel.vue
@@ -1,9 +1,20 @@
 <template>
     <div>
       <header>Encabezado Fijo</header>
-      <aside>Menú de Navegación</aside>
+      <VueSidebarMenuAkahon />
       <main>
         <router-view />
       </main>
     </div>
 </template>
+<script>
+  import VueSidebarMenuAkahon from "vue-sidebar-menu-akahon";
+  
+  export default {
+    name: 'AdminPanel.vue',
+    components: {VueSidebarMenuAkahon},
+    data() {
+      return {}
+    }
+  }
+</script>
