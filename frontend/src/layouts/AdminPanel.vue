eb67335ddf68252e8bb018e530484bf28a890d88
Franpall <skfranpal@gmail.com>
2025-04-27 13:06:01 -0400

    Creación de items del menú de administración

--- frontend/src/layouts/AdminPanel.vue
+++ frontend/src/layouts/AdminPanel.vue
@@ -1,7 +1,28 @@
 <template>
     <div>
       <header>Encabezado Fijo</header>
-      <VueSidebarMenuAkahon />
+      <VueSidebarMenuAkahon
+        :menuIcon="'bx bxs-store-alt'"
+        :menuSize="'50px'"
+        :menuTitle="'Decolorvers'"
+        :isUsedVueRouter="true"
+        @menu-item-clicked="fnRouteTo(link)"
+        :menuItems="
+          [
+            { name: 'Inicio', icon: 'bx bxs-home', link: '/admin/dashboard' },
+            { name: 'Productos', icon: 'bx bxs-shopping-bag', link: '/admin/products' },
+            { name: 'Categorias', icon: 'bx bxs-purchase-tag', link: '/admin/categories' },
+            { name: 'Pedidos', icon: 'bx bxs-cart-alt', link: '/admin/orders' },
+            { name: 'Ventas', icon: 'bx bx-wallet-alt', link: '/admin/sales' },
+            { name: 'Clientes', icon: 'bx bxs-user', link: '/admin/users' },
+            { name: 'Administradores', icon: 'bx bxs-user-voice', link: '/admin/adminis' },
+            { name: 'Supervisores', icon: 'bx bxs-user-voice', link: '/admin/supervisors' },
+            { name: 'Configuración', icon: 'bx bxs-cog', link: '/admin/settings' }
+          ]
+        "
+        :profileName="'Juan'"
+        :profileRole="'Jefe'"
+      />
       <main>
         <router-view />
       </main>
@@ -18,3 +39,11 @@
     }
   }
 </script>
+<style>
+.profile i{
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
