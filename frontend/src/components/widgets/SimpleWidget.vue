acb5dbc5bcd89f6a2ac6517e2632db4e7fb2e233
Franpall <skfranpal@gmail.com>
2025-04-30 19:53:03 -0400

    Estilo glassmorphism en los widgets

--- frontend/src/components/widgets/SimpleWidget.vue
+++ frontend/src/components/widgets/SimpleWidget.vue
@@ -35,7 +35,11 @@ export default {
         width: 100%;
         height: 100%;
         border-radius: 20px;
-        background: linear-gradient(to top right, #4b5563, #171717, #020617);
+        backdrop-filter: blur(16px) saturate(180%);
+        -webkit-backdrop-filter: blur(16px) saturate(180%);
+        background-color: rgba(17, 25, 40, 0.75);
+        border-radius: 12px;
+        border: 1px solid rgba(255, 255, 255, 0.125);
         margin: 0;
         font-family: roboto;
         height: 250px;
