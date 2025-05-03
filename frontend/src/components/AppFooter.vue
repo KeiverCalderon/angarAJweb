52af76d8466200283c7853a2e687ae91418d6cd5
Franpall <skfranpal@gmail.com>
2025-05-03 16:23:27 -0400

    Creación de footer con fecha auto-actualizable

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,60 @@
+<template>
+    <footer class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto text-center">
+            <p>&copy; {{ year }} Distribuciones Angar AJ. Todos los Derechos Reservados.</p>
+            <p>Algún problema? <a href="">contacta al soporte</a></p>
+        </div>
+        <div class="social-media">
+            <a href="https://www.facebook.com/DistribucionesAngarAJ" target="_blank" rel="noopener noreferrer"><i class='bx bxl-facebook'></i></a>
+            <a href="https://www.instagram.com/distribucionesangaraj/" target="_blank" rel="noopener noreferrer"><i class='bx bxl-instagram'></i></a>
+        </div>
+    </footer>
+</template>
+<script>
+import 'boxicons/css/boxicons.min.css';
+
+export default {
+    name: 'AppFooter',
+    data() {
+        return {
+            year: new Date().getFullYear(),
+        };
+    },
+    mounted() {
+        this.year = new Date().getFullYear();
+    },
+};
+</script>
+<style scoped>
+footer {
+    background-color: #2d3748; /* Gray-800 */
+    color: #fff;
+    padding: 1rem 0;
+}
+.container {
+    margin: 0 auto;
+    padding: 0 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+p {
+    margin: 0;
+    font-size: 0.875rem; /* Text-sm */
+}
+a {
+    color: #63b3ed; /* Blue-400 */
+    text-decoration: none;
+    transition: color 0.3s ease;
+}
+
+.social-media{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin-top: 10px;
+    font-size: 1.5rem;
+}
+</style>
--- frontend/src/layouts/ClientPanel.vue
+++ frontend/src/layouts/ClientPanel.vue
@@ -3,14 +3,22 @@
     <main>
         <router-view />
     </main>
+    <AppFooter />
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
 export default {
     name: 'ClientPanel',
     components: {
         AppHeader,
+        AppFooter,
     },
 }
 </script>
+<style scoped>
+main{
+    min-height: calc(100vh - 80px);
+}
+</style>
