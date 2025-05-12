4400d0a075e68d1d2119fedb5ea4ad82793fd0d2
Franpall <skfranpal@gmail.com>
2025-05-12 14:12:33 -0400

    Pruebas de niveles de sesión en el header

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,22 +1,46 @@
 <template>
     <header>
-            <h1><a href="/"><img src="../assets/logo.png" alt=""></a></h1>
-            <div class="buttons-area">
-                <a v-if="$route.path === '/cart' || $route.path === '/account'" href="/"><p class="a_text">Inicio</p><i class='bx bx-home' ></i></a>
-                <a v-else href="/cart"><p class="a_text">Carrito</p><i class='bx bx-cart' ></i></a>
-                <AppAccountMenu v-if="$route.path != '/account'"/>
-            </div>
+        <h1><a href="/"><img src="../assets/logo.png" alt=""></a></h1>
+        <div class="buttons-area">
+            <a v-if="$route.path === '/cart' || $route.path === '/account'" href="/"><p class="a_text">Inicio</p><i class='bx bx-home' ></i></a>
+            <a v-else href="/cart"><p class="a_text">Carrito</p><i class='bx bx-cart' ></i></a>
+            <a v-if="session===0" href="/login"><p class="a_text">Cuenta</p><i class='bx bx-user' ></i></a>
+            <a v-else-if="session===1 && $route.path != '/account'" href="/account"><p class="a_text">Perfil</p><i class='bx bx-user' ></i></a>
+            <a v-else href="/admin"><p class="a_text">Volver</p><i class='bx bx-reply' ></i></a>
+        </div>
+
+        <!-- Herramientas de desarrollo -->
+        <!-- Selector oculto -->
+        <div v-if="selectorVisible" class="hidden-selector" style="display:flex; align-items: center; justify-content: center; flex-direction: column; position: fixed; top: 90; left: 20; background-color: azure; z-index: 9999;">
+            <h2>DEVELOPER OPTIONS</h2>
+            <p>Nivel de sesión</p>
+            <input type="number" v-model="session" placeholder="LEVEL" min="0" max="4">
+            <p>Presione Z para cerrar</p>
+        </div>
+
     </header>
 </template>
 
 <script>
 import 'boxicons/css/boxicons.min.css';
-import AppAccountMenu from './AppAccountMenu.vue';
 
 export default {
     name: 'AppHeader',
-    components: {
-        AppAccountMenu
+    data() {
+        return {
+            session: 0, // 0: No logueado, 1: Cliente, 2: Admin, 3: Supervisor, 4: Jefe
+            selectorVisible: false, // TEST
+        };
+    },
+    methods: {
+        toggleSelector(event) { // TEST
+            if (event.key === 'z') { // TEST
+                this.selectorVisible = !this.selectorVisible; // TEST
+            } // TEST
+        }, // TEST
+    },
+    mounted() {
+        window.addEventListener('keydown', this.toggleSelector);// TEST
     },
 };
 </script>
